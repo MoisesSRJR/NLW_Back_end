eda9b294e7c2ed4fff0743b84baf9451ca988e18
MoisesSRJR <moises.sampaiojr@outlook.com>
2022-10-26 00:24:24 -0300

    added new routes

--- src/server.ts
+++ src/server.ts
@@ -2,7 +2,26 @@ import express from 'express'
 
 const app = express()
 
-app.get('/ads', (request, response) => {
+app.get('/games', (request, response) => {
+    return response.json([])
+})
+
+app.post('/ads', (request, response) => {
+    return response.json([])
+})
+
+app.get('/games/:id/ads', (request, response) => {
+    // const gameId = request.params.id;
+
+    return response.json([
+        {id: 1, name: 'nome 01'},
+        {id: 2, name: 'nome 02'},
+        {id: 3, name: 'nome 03'},
+        {id: 4, name: 'nome 04'},
+    ])
+})
+
+app.get('/ads/:id/discord', (request, response) => {
     return response.json([
         {id: 1, name: 'nome 01'},
         {id: 2, name: 'nome 02'},
@@ -13,3 +32,4 @@ app.get('/ads', (request, response) => {
 
 app.listen(3333) //localhost:3333/ads
 
+// 43:07 
